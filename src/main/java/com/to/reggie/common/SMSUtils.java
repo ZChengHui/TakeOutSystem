76dd1b24f7ea345cb1de6c662424adc81e2aaec4
zch <2783300744@qq.com>
2022-08-10 22:14:30 +0800

    更新README文件

--- src/main/java/com/to/reggie/common/SMSUtils.java
+++ src/main/java/com/to/reggie/common/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tJ3B6x4sihDErHQ2LB9", "FLTygzH9JCqFzsGSUqFu5WuE3cQLFN");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
